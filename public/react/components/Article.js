cd98e0ffc7f72708e2b07de2acf68571d9d9faf9
nimonian <nimon@hotmail.co.uk>
2023-05-18 16:30:27 +0100

    fixes setPage bug

--- public/react/components/Article.js
+++ public/react/components/Article.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from 'react'
 
-export const Article = ({article}) => {
-
-    {console.log(article)}
-    {console.log(article.title)}
-//<h3>{article.authorId}</h3> need to get author name by author id
-  return <>
-    <h3>{article.title}</h3>
-    <h3>{article.content}</h3>
-    <h3>{article.tags}</h3>
-    <h3>{article.createdAt}</h3>
-  </>
-} 
+export const Article = ({ article }) => {
+  //<h3>{article.authorId}</h3> need to get author name by author id
+  return (
+    <>
+      <h3>{article.title}</h3>
+      <h3>{article.content}</h3>
+      <h3>{article.tags}</h3>
+      <h3>{article.createdAt}</h3>
+    </>
+  )
+}
